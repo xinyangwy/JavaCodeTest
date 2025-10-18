a1939f0bfe567d6db0c30b38b48de20cfe5c0ea2
wuzilong <wzlhello@qq.com>
2025-10-18 22:30:54 +0800

    add：岛屿问题

---
+++ Leetcode2025Fall/src/s200.java
@@ -0,0 +1,29 @@
+/**
+ * @Auther: WuZilong
+ * @Date: 2025/10/17 22:57
+ * @Description:
+ */
+public class s200 {
+    int [][]dirs=new int[][]{{0,1},{-1,0},{0,-1},{1,0}};
+    public int numIslands(char[][] grid) {
+        int res=0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j]=='1'){
+                    dfs(grid,i,j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+    private void dfs(char[][] grid,int i,int j){
+        if(i<0||i>=grid.length||j<0||j>=grid[0].length||grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j]='0';
+        for (int[] dir : dirs) {
+            dfs(grid,i+dir[0],j+dir[1]);
+        }
+    }
+}
